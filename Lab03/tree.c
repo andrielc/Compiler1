f37b833ea6a3d372022a206c454a16029b3b40b4
andrielc <andriel.biagioni@gmail.com>
2016-10-26 22:13:27 -0200

    Create tree.c

---
+++ Lab03/tree.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+const char *CAL[20]={
+	"C_FUNCTION",
+	"C_BLOCK",
+	"C_LABEL",
+	"C_TYPE",
+	"C_VARIABLE",
+	"C_POINTER",
+	"C_STATEMENT",
+	"C_IF",
+	"C_RETURN",
+	"C_GOTO",
+	"C_BIN_EXPR",
+	"C_EXPR",
+	"C_NEG",
+	"C_ASSIGN",
+	"C_WHILE",
+	"C_FUNCTION_CALL",
+	"C_IDENT",
+	"C_INTEGER",
+	"C_NOT",
+	"C_OPERATOR"};
+
