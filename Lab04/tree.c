24dcda7e8f79412d78fcacd1b9d7bd8d1c8347a3
andrielc <andriel.biagioni@gmail.com>
2016-12-05 23:54:24 -0200

    Update tree.c

--- Lab04/tree.c
+++ Lab04/tree.c
@@ -1,45 +1,146 @@
-#ifndef _TREE_H_
-#define _TREE_H_
-typedef enum{
-	C_FUNCTION=0,
-	C_BLOCK,
-	C_LABEL,
-	C_TYPE,
-	C_VARIABLE,
-	C_POINTER,
-	C_STATEMENT,
-	C_IF,
-	C_RETURN,
-	C_GOTO,
-	C_BIN_EXPR,
-	C_EXPR,
-	C_NEG,
-	C_ASSIGN,
-	C_WHILE,
-	C_FUNCTION_CALL,
-	C_IDENT,
-	C_INTEGER,
-	C_NOT,
-	C_OPERATOR
-} Categ;
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "tree.h"
 
-typedef struct _treeNode{
-	Categ categ;
-	struct _treeNode *next; //list
-	struct _treeNode *comps[8];
-	char *str;   // IDENT, INTEGER or operator
-} TreeNode, *TreeNodePtr;
+const char *CATEGS[20]={
+	"C_FUNCTION",
+	"C_BLOCK",
+	"C_LABEL",
+	"C_TYPE",
+	"C_VARIABLE",
+	"C_POINTER",
+	"C_STATEMENT",
+	"C_IF",
+	"C_RETURN",
+	"C_GOTO",
+	"C_BIN_EXPR",
+	"C_EXPR",
+	"C_NEG",
+	"C_ASSIGN",
+	"C_WHILE",
+	"C_FUNCTION_CALL",
+	"C_IDENT",
+	"C_INTEGER",
+	"C_NOT",
+	"C_OPERATOR"
+};
 
-void *getTree();  // returns any pointer
-TreeNodePtr genNode2(Categ cat, char *str);
-TreeNodePtr genNode3(Categ cat, char *str, TreeNodePtr p1);
-TreeNodePtr genNode4(Categ cat, char *str, TreeNodePtr p1, TreeNodePtr p2);
-TreeNodePtr genNode5(Categ cat, char *str, TreeNodePtr p1, TreeNodePtr p2, TreeNodePtr p3);
-TreeNodePtr genNode6(Categ cat, char *str, TreeNodePtr p1, TreeNodePtr p2, TreeNodePtr p3,TreeNodePtr p4);
-TreeNodePtr genNode7(Categ cat, char *str, TreeNodePtr p1, TreeNodePtr p2, TreeNodePtr p3,TreeNodePtr p4,TreeNodePtr p5);
-TreeNodePtr inversao(TreeNodePtr tnp);
-int stackHeight();
-void counts(void *p, int *functions, int *funcalls, int *whiles, int *ifs, int *bin);
-void dumpTree(TreeNode *p, int indent,char *tab);
+extern TreeNodePtr tree;
+extern int stackheight;
 
-#endif
+int stackHeight() {
+	return stackheight;
+}
+
+void *getTree() {
+	return tree;
+}
+
+TreeNodePtr genNode2(Categ cat, char *str){
+
+	TreeNodePtr np = malloc(sizeof(TreeNode));
+	np->categ = cat;
+	np->next = NULL;
+	np->str = str;
+
+	return np;
+	
+}
+
+TreeNodePtr genNode3(Categ cat, char *str,TreeNodePtr p1){
+	
+	TreeNodePtr np = malloc(sizeof(TreeNode));
+	np->categ = cat;
+	np->next = NULL;
+	np->str = str;
+
+	np->comps[0] = p1;
+	return np;
+}
+
+TreeNodePtr genNode4(Categ cat, char *str,TreeNodePtr p1, TreeNodePtr p2){
+	
+	TreeNodePtr np = malloc(sizeof(TreeNode));
+	np->categ = cat;
+	np->next = NULL;
+	np->str = str;
+
+	np->comps[0] = p1;
+	np->comps[1] = p2;
+	return np;
+}
+
+TreeNodePtr genNode5(Categ cat, char *str,TreeNodePtr p1, TreeNodePtr p2, TreeNodePtr p3){
+	
+	TreeNodePtr np = malloc(sizeof(TreeNode));
+	np->categ = cat;
+	np->next = NULL;
+	np->str = str;
+	
+	np->comps[0] = p1;
+	np->comps[1] = p2;
+	np->comps[2] = p3;
+	return np;
+}
+
+TreeNodePtr genNode6(Categ cat, char *str,TreeNodePtr p1, TreeNodePtr p2, TreeNodePtr p3,TreeNodePtr p4){
+
+	TreeNodePtr np = malloc(sizeof(TreeNode));
+	np->categ = cat;
+	np->next = NULL;
+	np->str = str;
+
+	np->comps[0] = p1;
+	np->comps[1] = p2;
+	np->comps[2] = p3;
+	np->comps[3] = p4;
+	return np;
+}
+
+TreeNodePtr genNode7(Categ cat, char *str,TreeNodePtr p1, TreeNodePtr p2, TreeNodePtr p3,TreeNodePtr p4,TreeNodePtr p5){
+	TreeNodePtr np = malloc(sizeof(TreeNode));
+	np->categ = cat;
+	np->next = NULL;
+	np->str = str;
+
+	np->comps[0] = p1;
+	np->comps[1] = p2;
+	np->comps[2] = p3;
+	np->comps[3] = p4;
+	np->comps[4] = p5;
+	return np;
+}
+
+
+void dumpTree(TreeNodePtr p, int indent, char *tab){
+	int i=0;
+	
+	while(i++<indent)
+		 printf("%s",tab);
+	
+	if(p->str) 
+		printf("Data: %s ",p->str);
+	fflush(stdout);
+	
+	printf("Categ: %s\n",CATEGS[p->categ]);
+	fflush(stdout);
+	
+	i=0;
+	while(i<6 && p->comps[i])
+		dumpTree(p->comps[i++],indent+1,tab);
+	
+	if(p->next)
+		dumpTree(p->next,indent,tab);
+}
+
+TreeNodePtr inversao (TreeNodePtr tnp) {
+	TreeNodePtr aux, np = NULL;
+	while(tnp){
+		aux = tnp->next;
+		tnp->next = np;
+		np = tnp;
+		tnp = aux;
+	}
+	return np;
+}
